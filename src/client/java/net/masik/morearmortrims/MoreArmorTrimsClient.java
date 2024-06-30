47467599f776d954cbfdc53dd91a4fa43e451f13
Maks <maksim.volokitin.05@gmail.com>
2024-06-30 11:49:59 -0700

    1.21

---
+++ src/client/java/net/masik/morearmortrims/MoreArmorTrimsClient.java
@@ -0,0 +1,10 @@
+package net.masik.morearmortrims;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreArmorTrimsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
