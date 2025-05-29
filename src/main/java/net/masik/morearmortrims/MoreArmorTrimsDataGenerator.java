efc423a56d78a8418c7c78d8e796f47c1cddd985
Maks <maksim.volokitin.05@gmail.com>
2025-05-29 12:33:49 -0700

    port to 1.21.4

--- src/main/java/net/masik/morearmortrims/MoreArmorTrimsDataGenerator.java
+++ src/main/java/net/masik/morearmortrims/MoreArmorTrimsDataGenerator.java
@@ -2,8 +2,6 @@ package net.masik.morearmortrims;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.masik.morearmortrims.datagen.ModItemTagProvider;
-import net.masik.morearmortrims.datagen.ModModelProvider;
 import net.masik.morearmortrims.datagen.ModRecipeProvider;
 
 public class MoreArmorTrimsDataGenerator implements DataGeneratorEntrypoint {
@@ -12,8 +10,6 @@ public class MoreArmorTrimsDataGenerator implements DataGeneratorEntrypoint {
 
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-		pack.addProvider(ModItemTagProvider::new);
-		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
 
 	}
